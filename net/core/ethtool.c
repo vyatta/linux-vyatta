ce4dbe0445f249f4fc077124d3a87f0448cf2e20
Stephen Hemminger <stephen.hemminger@vyatta.com>
2008-02-25 10:11:16 -0800

    erase wake-on-lan password for non-root
    When doing get wake on lan settings, zero out the password for
    non-root users, to avoid leaking potential security settings.

--- net/core/ethtool.c
+++ net/core/ethtool.c
@@ -212,6 +212,9 @@ static int ethtool_get_wol(struct net_device *dev, char __user *useraddr)
 
 	dev->ethtool_ops->get_wol(dev, &wol);
 
+	if (!capable(CAP_NET_ADMIN))
+		memset(wol.sopass, 0, SOPASS_MAX);
+
 	if (copy_to_user(useraddr, &wol, sizeof(wol)))
 		return -EFAULT;
 	return 0;
