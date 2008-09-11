e4a9b52c7774474cd99d3559bc09671cd7ab726e
Stephen Hemminger <stephen.hemminger@vyatta.com>
2008-09-11 15:48:54 -0700

    Fix prototypes from unionfs changes

--- include/linux/splice.h
+++ include/linux/splice.h
@@ -76,9 +76,9 @@ extern long vfs_splice_to(struct file *in, loff_t *ppos,
 			  struct pipe_inode_info *pipe, size_t len,
 			  unsigned int flags);
 
-extern long do_splice_from(struct pipe_inode_info *pipe, struct file *out,
+extern long vfs_splice_from(struct pipe_inode_info *pipe, struct file *out,
 			   loff_t *ppos, size_t len, unsigned int flags);
-extern long do_splice_to(struct file *in, loff_t *ppos,
+extern long vfs_splice_to(struct file *in, loff_t *ppos,
 			 struct pipe_inode_info *pipe, size_t len,
 			 unsigned int flags);
 
