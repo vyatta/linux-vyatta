0fcfd5b222bdb5edb06c1deeaf1eccdf2886364f
Stephen Hemminger <stephen.hemminger@vyatta.com>
2008-12-09 14:46:55 -0800

    Revert "lib/idr.c: fix rcu related race with idr_find"
    This reverts commit 228d6b8f4ca2c057e9c63559f72af111b4356bd3.
    
    Several people have reported regressions with 2.6.27.8 because of this
    patch.

--- include/linux/idr.h
+++ include/linux/idr.h
@@ -52,14 +52,13 @@ struct idr_layer {
 	unsigned long		 bitmap; /* A zero bit means "space here" */
 	struct idr_layer	*ary[1<<IDR_BITS];
 	int			 count;	 /* When zero, we can release it */
-	int			 layer;	 /* distance from leaf */
 	struct rcu_head		 rcu_head;
 };
 
 struct idr {
 	struct idr_layer *top;
 	struct idr_layer *id_free;
-	int		  layers; /* only valid without concurrent changes */
+	int		  layers;
 	int		  id_free_cnt;
 	spinlock_t	  lock;
 };
