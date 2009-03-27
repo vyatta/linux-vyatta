b3bd647203320a1c9baee7439a11f57ea12b75ab
Stephen Hemminger <stephen.hemminger@vyatta.com>
2009-03-27 16:01:44 -0700

    Cleanup some leftover debris from merge

--- arch/x86/kernel/vmi_32.c
+++ arch/x86/kernel/vmi_32.c
@@ -331,16 +331,6 @@ static void vmi_pgd_free(struct mm_struct *mm, pgd_t *pgd)
 }
 
 /*
- * We use the pgd_free hook for releasing the pgd page:
- */
-static void vmi_pgd_free(struct mm_struct *mm, pgd_t *pgd)
-{
-	unsigned long pfn = __pa(pgd) >> PAGE_SHIFT;
-
-	vmi_ops.release_page(pfn, VMI_PAGE_L2);
-}
-
-/*
  * Helper macros for MMU update flags.  We can defer updates until a flush
  * or page invalidation only if the update is to the current address space
  * (otherwise, there is no flush).  We must check against init_mm, since
