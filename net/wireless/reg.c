0dd8e8e0181ac86c4b6d3362611fbb7536e45e71
Stephen Hemminger <stephen.hemminger@vyatta.com>
2009-07-22 11:53:33 -0700

    Align vyatta kernel with upstream
    These files don't need to be changed from upstream

--- net/wireless/reg.c
+++ net/wireless/reg.c
@@ -2189,13 +2189,6 @@ int regulatory_init(void)
 #endif
 	}
 
-	/*
-	 * This ensures last_request is populated once modules
-	 * come swinging in and calling regulatory hints and
-	 * wiphy_apply_custom_regulatory().
-	 */
-	flush_scheduled_work();
-
 	return 0;
 }
 
