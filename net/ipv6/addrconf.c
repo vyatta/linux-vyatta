458012fc2b7945422cf417a9533bc5771e34bdac
Stephen Hemminger <stephen.hemminger@vyatta.com>
2010-02-26 10:52:59 -0800

    IPv6: addrconf dad timer unnecessary bh_disable
    Timer code runs in bottom half, so there is no need for
    using _bh form of locking.
    
    Signed-off-by: Stephen Hemminger <shemminger@vyatta.com>

--- net/ipv6/addrconf.c
+++ net/ipv6/addrconf.c
@@ -2864,20 +2864,20 @@ static void addrconf_dad_timer(unsigned long data)
 	struct inet6_dev *idev = ifp->idev;
 	struct in6_addr mcaddr;
 
-	read_lock_bh(&idev->lock);
-	if (idev->dead) {
-		read_unlock_bh(&idev->lock);
+	read_lock(&idev->lock);
+	if (idev->dead || !(idev->if_flags & IF_READY)) {
+		read_unlock(&idev->lock);
 		goto out;
 	}
-	spin_lock_bh(&ifp->lock);
+	spin_lock(&ifp->lock);
 	if (ifp->probes == 0) {
 		/*
 		 * DAD was successful
 		 */
 
 		ifp->flags &= ~(IFA_F_TENTATIVE|IFA_F_OPTIMISTIC|IFA_F_DADFAILED);
-		spin_unlock_bh(&ifp->lock);
-		read_unlock_bh(&idev->lock);
+		spin_unlock(&ifp->lock);
+		read_unlock(&idev->lock);
 
 		addrconf_dad_completed(ifp);
 
@@ -2886,8 +2886,8 @@ static void addrconf_dad_timer(unsigned long data)
 
 	ifp->probes--;
 	addrconf_mod_timer(ifp, AC_DAD, ifp->idev->nd_parms->retrans_time);
-	spin_unlock_bh(&ifp->lock);
-	read_unlock_bh(&idev->lock);
+	spin_unlock(&ifp->lock);
+	read_unlock(&idev->lock);
 
 	/* send a neighbour solicitation for our addr */
 	addrconf_addr_solict_mult(&ifp->addr, &mcaddr);
