b23c18a26354ab8dd552b710c8d291ca5954549f
Stephen Hemminger <stephen.hemminger@vyatta.com>
2010-05-03 13:56:05 -0700

    Fix duplicate definiton.

--- fs/squashfs/squashfs_fs.h
+++ fs/squashfs/squashfs_fs.h
@@ -426,14 +426,4 @@ struct squashfs_xattr_header {
 	__le32			size;
 };
 
-struct squashfs_xattr_entry {
-	__le32			name_len;
-	__le32			value_len;
-	char			name_and_value_data[0];
-};
-
-struct squashfs_xattr_header {
-	__le32			size;
-};
-
 #endif
