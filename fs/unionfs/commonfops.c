8cbcdb692b36b430f802db2a360dbcba53debd63
Stephen Hemminger <stephen.hemminger@vyatta.com>
2011-02-02 13:53:59 -0800

    Fix unionfs for file_ops change
    The code to handle compat_ioctl had wrong args.

--- fs/unionfs/commonfops.c
+++ fs/unionfs/commonfops.c
@@ -740,10 +740,8 @@ static long do_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 	if (lower_file->f_op->unlocked_ioctl) {
 		err = lower_file->f_op->unlocked_ioctl(lower_file, cmd, arg);
 #ifdef CONFIG_COMPAT
-	} else if (lower_file->f_op->ioctl) {
-		err = lower_file->f_op->compat_ioctl(
-			lower_file->f_path.dentry->d_inode,
-			lower_file, cmd, arg);
+	} else if (lower_file->f_op->compat_ioctl) {
+		err = lower_file->f_op->compat_ioctl(lower_file, cmd, arg);
 #endif
 	}
 
