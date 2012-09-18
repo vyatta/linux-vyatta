a89c84a5fad85769f67315c974aa5cdf8ebba74b
Stephen Hemminger <shemminger@vyatta.com>
2012-09-18 09:53:14 -0700

    Add link filter sysctl
    Vyatta specific extension to allow control over link events

--- include/linux/inetdevice.h
+++ include/linux/inetdevice.h
@@ -39,6 +39,8 @@ enum
 	IPV4_DEVCONF_SRC_VMARK,
 	IPV4_DEVCONF_PROXY_ARP_PVLAN,
 	IPV4_DEVCONF_ROUTE_LOCALNET,
+
+	IPV4_DEVCONF_LINKFILTER,	/* Vyatta only */
 	__IPV4_DEVCONF_MAX
 };
 
@@ -145,6 +147,7 @@ static inline void ipv4_devconf_setall(struct in_device *in_dev)
 #define IN_DEV_ARP_ANNOUNCE(in_dev)	IN_DEV_MAXCONF((in_dev), ARP_ANNOUNCE)
 #define IN_DEV_ARP_IGNORE(in_dev)	IN_DEV_MAXCONF((in_dev), ARP_IGNORE)
 #define IN_DEV_ARP_NOTIFY(in_dev)	IN_DEV_MAXCONF((in_dev), ARP_NOTIFY)
+#define IN_DEV_LINKFILTER(in_dev)	IN_DEV_MAXCONF((in_dev), LINKFILTER)
 
 struct in_ifaddr {
 	struct hlist_node	hash;
